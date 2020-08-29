d08e2cf22e4fb8a5c0e234ab66cb906a1ed96472
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-08-29 13:01:23 +0300

    fix active link styles

--- src/shared/components/Navigation/NavLinks/NavLinks.js
+++ src/shared/components/Navigation/NavLinks/NavLinks.js
@@ -6,12 +6,18 @@ import classes from './NavLinks.module.scss';
 const NavLinks = () => (
   <ul className={classes['nav-links']}>
     <li>
-      <NavLink to="/" exact>
+      <NavLink activeClassName={classes.active} to="/" exact>
         ALL USERS
       </NavLink>
-      <NavLink to="/u1/places">MY PLACES</NavLink>
-      <NavLink to="/places/new">ADD PLACE</NavLink>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
+      <NavLink activeClassName={classes.active} to="/u1/places">
+        MY PLACES
+      </NavLink>
+      <NavLink activeClassName={classes.active} to="/places/new">
+        ADD PLACE
+      </NavLink>
+      <NavLink activeClassName={classes.active} to="/auth">
+        AUTHENTICATE
+      </NavLink>
     </li>
   </ul>
 );
