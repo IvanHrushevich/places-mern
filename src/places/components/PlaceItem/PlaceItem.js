1759c281c5353c054e675b6be5c76ac5a4c80385
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2020-11-01 01:39:30 +0300

    add modal

--- src/places/components/PlaceItem/PlaceItem.js
+++ src/places/components/PlaceItem/PlaceItem.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './PlaceItem.module.scss';
-import { Button, Card } from '../../../shared/components';
+import { Button, Card, Modal } from '../../../shared/components';
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className={classes['place-item']}>
-      <Card className={classes['place-item__content']}>
-        <div className={classes['place-item__image']}>
-          <img src={props.image} alt={props.title} />
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className="map-container">
+          <h1>The map</h1>
         </div>
+      </Modal>
+      <li className={classes['place-item']}>
+        <Card className={classes['place-item__content']}>
+          <div className={classes['place-item__image']}>
+            <img src={props.image} alt={props.title} />
+          </div>
 
-        <div className={classes['place-item__info']}>
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
+          <div className={classes['place-item__info']}>
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
 
-        <div className={classes['place-item__actions']}>
-          <Button inverse>VIEW ON MAP</Button>
-          <Button to={`/places/${props.id}`}>EDIT</Button>
-          <Button danger>DELETE</Button>
-        </div>
-      </Card>
-    </li>
+          <div className={classes['place-item__actions']}>
+            <Button inverse onClick={openMapHandler}>
+              VIEW ON MAP
+            </Button>
+            <Button to={`/places/${props.id}`}>EDIT</Button>
+            <Button danger>DELETE</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
--- src/shared/components/UI/index.js
+++ src/shared/components/UI/index.js
@@ -1,5 +1,6 @@
 import Avatar from './Avatar/Avatar';
 import Card from './Card/Card';
 import Backdrop from './Backdrop/Backdrop';
+import Modal from './Modal/Modal';
 
-export { Avatar, Backdrop, Card };
+export { Avatar, Backdrop, Card, Modal };
